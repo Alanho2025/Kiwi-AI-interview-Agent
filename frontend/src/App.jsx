71724f45be4beb0b754d32ea5c40dc18ca3e3e57
yuning-fan <yfan707@aucklanduni.ac.nz>
2026-04-05 05:46:48 +1200

    Add Home page, GoogleOAuth login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AnalyzePage } from './pages/AnalyzePage.jsx';
 import { InterviewPage } from './pages/InterviewPage.jsx';
+import Login from './pages/Login.jsx'; 
+import HomePage from './pages/HomePage.jsx';
 
 export default function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<HomePage />} />
+        {/* 下面是你原有的路由保持不变 */}
         <Route path="/" element={<AnalyzePage />} />
         <Route path="/interview/:sessionId" element={<InterviewPage />} />
       </Routes>
